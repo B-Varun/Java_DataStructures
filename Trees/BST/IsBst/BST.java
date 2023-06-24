cf2690148a931683b3cc0e2e484b956e6a0015e7
B-Varun <bvarun662@gmail.com>
2023-06-24 13:32:34 -0700

    Adding all Depth First Traversal Algorithms in the same file

---
+++ Trees/BST/IsBst/BST.java
@@ -0,0 +1,23 @@
+package com.java.learn.ds.tree.bst;
+
+import com.java.learn.ds.node.Node;
+
+public class BST{
+
+public Node rootPtr = null;
+
+public Node insert(Node root, Node node){
+ if(root == null)
+	root = node;
+ else if(root.data >= node.data)
+	root.left = insert(root.left, node);
+ else
+	root.right = insert(root.right, node);
+ return root;
+}
+
+public boolean isEmpty(){
+ return (rootPtr == null) ? true : false;
+}
+
+}
---
+++ Trees/BST/IsBst/IsBst.java
@@ -0,0 +1,59 @@
+package com.java.learn.ds.tree.bst.isbst;
+
+import com.java.learn.ds.node.Node;
+import com.java.learn.ds.tree.bst.BST;
+
+public class IsBst{
+
+public static void main(String... tre){
+ BST bst = new BST();
+ int[] arr = new int[]{2, 1, 3};
+
+ for(int i=0; i<arr.length; i++)
+    bst.rootPtr = bst.insert(bst.rootPtr, new Node(arr[i]));
+ 
+ System.out.println("Inorder of the BST is :");
+ inOrder(bst.rootPtr);
+
+ System.out.println("\n\nPreorder of the BST is :");
+ preOrder(bst.rootPtr);
+
+ System.out.println("\n\nPostorder of the BST is :");
+ postOrder(bst.rootPtr);
+}
+
+public static void inOrder(Node root){
+ if(root == null)
+	return;
+ else{
+	inOrder(root.left);
+	System.out.print(root.data+"\t");
+	inOrder(root.right);
+ }
+}
+
+public static void preOrder(Node root){
+ if(root == null)
+	return;
+ else{
+	System.out.print(root.data+"\t");
+	preOrder(root.left);
+	preOrder(root.right);
+ }
+}
+
+public static void postOrder(Node root){
+ if(root == null)
+	return;
+ else{
+	postOrder(root.left);
+	postOrder(root.right);
+	System.out.print(root.data+"\t");
+ }
+}
+
+public static boolean isBST(Node root){
+ return false;
+}
+
+}
