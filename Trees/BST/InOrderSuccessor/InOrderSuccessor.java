af808aff0c7d45add96295d264cd063e1a865ccb
B-Varun <bvarun662@gmail.com>
2023-06-28 13:21:50 -0700

    Altered code for InOrderSuccessor

--- Trees/BST/InOrderSuccessor/InOrderSuccessor.java
+++ Trees/BST/InOrderSuccessor/InOrderSuccessor.java
@@ -11,21 +11,31 @@ public static void main(String... tree){
  for(int i=0; i<arr.length; i++)
 	bst.rootPtr = bst.insert(bst.rootPtr, new Node(arr[i]));
 
- System.out.println("InOrderSuccessor of 100 is : "+inOrderSuccessor(bst.rootPtr, 100));
+ System.out.println("InOrderSuccessor of 100 is : "+inOrderSuccessor(bst.rootPtr, bst.rootPtr, 100));
 
- System.out.println("InOrderSuccessor of 50 is : "+inOrderSuccessor(bst.rootPtr, 50));
+ System.out.println("InOrderSuccessor of 50 is : "+inOrderSuccessor(bst.rootPtr, bst.rootPtr, 50));
+
+ System.out.println("InOrderSuccessor of 90 is : "+inOrderSuccessor(bst.rootPtr, bst.rootPtr, 90));
+
+ System.out.println("InOrderSuccessor of 99 is : "+inOrderSuccessor(bst.rootPtr, bst.rootPtr, 99));
+
+ System.out.println("InOrderSuccessor of 150 is : "+inOrderSuccessor(bst.rootPtr, bst.rootPtr, 150));
 
 }
 
-public static int inOrderSuccessor(Node root, int rootVal){
- if(root == null)
+public static int inOrderSuccessor(Node root, Node temp, int rootVal){
+ if(temp == null)
 	return -1;
- else if(root.data > rootVal)
-	return inOrderSuccessor(root.left, rootVal);
- else if(root.data < rootVal)
-	return inOrderSuccessor(root.right, rootVal);
- else
-	return successor(root.right);
+ else if(temp.data > rootVal)
+	return inOrderSuccessor(root, temp.left, rootVal);
+ else if(temp.data < rootVal)
+	return inOrderSuccessor(root, temp.right, rootVal);
+ else{
+	if(temp.right !=null)
+		return successor(temp.right);
+	else
+		return nearestAncestor(root, temp);
+ }
 }
 
  public static int successor(Node root){
@@ -33,6 +43,23 @@ public static int inOrderSuccessor(Node root, int rootVal){
 	return root.data;
   else
 	return successor(root.left);
+ }
+
+ public static int nearestAncestor(Node root, Node temp){
+   Node successor = null;
+   Node curr = root;
+
+  while(curr != temp){
+   if(curr.data > temp.data){
+	successor = curr;
+	curr = curr.left;
+   }
+   else if(curr.data < temp.data){
+	successor = curr;
+	curr = curr.right;
+   }	
+  }
+  return successor.data;
  }
 
 }
