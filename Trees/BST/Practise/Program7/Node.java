c95c215c766a662db023aae7a9dac889ab96ac5d
B-Varun <bvarun662@gmail.com>
2023-07-13 23:48:16 -0700

    Practise Tree Program

---
+++ Trees/BST/Practise/Program7/Node.java
@@ -0,0 +1,11 @@
+package com.java.practise.node;
+
+public class Node{
+public int data;
+public Node next, prev;
+
+public Node(int d){
+ data = d;
+ next = prev = null;
+}
+}
---
+++ Trees/BST/Practise/Program7/Queue.java
@@ -0,0 +1,49 @@
+package com.java.practise.ds.queue;
+import com.java.practise.node.Node; 
+public class Queue{
+public int rear, front;
+private int size=10;
+public Node[] arr;
+
+public Queue(){
+arr = new Node[size];
+front = rear = -1;
+}
+
+public void enqueue(Node node){
+ if(isFull())
+    return;
+ else if(isEmpty()){
+   front = rear = 0;
+   arr[rear] = node;
+  }
+ arr[++rear] = node; 
+}
+
+public Node dequeue(){
+ if(isEmpty())
+    return null;
+ else if(front == rear){
+    Node temp = arr[front];
+    front = rear = -1;
+    return temp;
+  }
+ return arr[front++];
+}
+
+public boolean isFull(){
+ return ((rear+1) == size) ? true : false;
+}
+
+public boolean isEmpty(){
+ return (rear == -1 && front == -1) ? true : false;
+}
+
+public Node front(){
+ return isEmpty() ? null : arr[front];
+}
+
+public Node rear(){
+ return isEmpty() ? null : arr[rear];
+}
+}
