6b75625385042372dd24bf8431ad37e9cd4e7bc9
B-Varun <bvarun662@gmail.com>
2024-04-09 23:56:22 -0700

    Adding other functionalities

--- 2024/LinkedLists/LinkedList.java
+++ 2024/LinkedLists/LinkedList.java
@@ -14,22 +14,54 @@ this.next = next;
 public class LinkedList{
 Node node=null, head = null;
 
-public LinkedList(int d, Node n){
- node = new Node(d, n);
+/*public void insert(int data){
+Node h;
+if( h == null)
+  h = new Node(data);
+else{
+ while(h!=null){
+   h = h.next;
+ }
 }
-
-public void insert(){}
+}
+*/
 
 public void delete(){}
-
-public void setHead(){}
 
 public Node head(){
  return head;
 }
 
+public boolean isEmpty(){
+return (head == null) ? true : false;
+}
+
+public void printList(){
+ Node h = head;
+ System.out.println("\n\nElements of the list are : ");
+ while(h!=null){
+  System.out.println(h.data);
+  h = h.next;
+ }
+}
+
 public static void main(String... args){
 System.out.println("Linked List");
+LinkedList list = new LinkedList();
+System.out.println("Is the list empty : "+list.isEmpty());
+list.printList();
+/*
+System.out.println(list.head());
+list.insert(20);
+list.insert(30);
+list.insert(40);
+list.insert(50);
+list.insert(60);
+list.insert(70);
+list.printList();
+*/
+System.out.println("Is the list empty : "+list.isEmpty());
+
 }
 
 }
